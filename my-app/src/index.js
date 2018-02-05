f4e9d0bd2a72ceba7b9e3f691a827aca93af652a
GupitaGupi <gupitaprameswari@gmail.com>
2018-02-05 15:59:40 +0700

    Remove my App
    Accidentally put original my app folder in gitHub folder

--- my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Routers from './routers';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Routers />, document.getElementById('root'));
-registerServiceWorker();
